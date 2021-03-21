697554173649dfa194e2679baa9bc53435c98ec9
Tiffany White <hey@tiffanyrwhite.com>
2021-03-21 02:26:06 -0400

    Update something

---
+++ config/custom_components/youtube/sensor.py
@@ -0,0 +1,165 @@
+"""
+A platform which give you info about the newest video on a channel.
+
+For more details about this component, please refer to the documentation at
+https://github.com/custom-components/youtube
+"""
+
+import html
+import logging
+import async_timeout
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
+from homeassistant.helpers.aiohttp_client import async_create_clientsession
+from homeassistant.components.sensor import PLATFORM_SCHEMA
+from homeassistant.helpers.entity import Entity
+from dateutil.parser import parse
+import re
+
+CONF_CHANNEL_ID = 'channel_id'
+
+ICON = 'mdi:youtube'
+
+BASE_URL = 'https://www.youtube.com/feeds/videos.xml?channel_id={}'
+CHANNEL_LIVE_URL = 'https://www.youtube.com/channel/{}'
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Required(CONF_CHANNEL_ID): cv.string,
+})
+
+_LOGGER = logging.getLogger(__name__)
+
+
+async def async_setup_platform(
+        hass, config, async_add_entities, discovery_info=None):  # pylint: disable=unused-argument
+    """Setup sensor platform."""
+    channel_id = config['channel_id']
+    session = async_create_clientsession(hass)
+    try:
+        url = BASE_URL.format(channel_id)
+        async with async_timeout.timeout(10, loop=hass.loop):
+            response = await session.get(url)
+            info = await response.text()
+        name = html.parser.HTMLParser().unescape(
+            info.split('<title>')[1].split('</')[0])
+    except Exception:  # pylint: disable=broad-except
+
+        name = None
+
+    if name is not None:
+        async_add_entities([YoutubeSensor(channel_id, name, session)], True)
+
+class YoutubeSensor(Entity):
+    """YouTube Sensor class"""
+    def __init__(self, channel_id, name, session):
+        self._state = None
+        self.session = session
+        self._image = None
+        self.stars = 0
+        self.views = 0
+        self.stream = False
+        self.live = False
+        self._name = name
+        self.channel_id = channel_id
+        self.url = None
+        self.published = None
+        self.channel_live = False
+        self.channel_image = None
+        self.expiry = parse('01 Jan 1900 00:00:00 UTC')
+
+    async def async_update(self):
+        """Update sensor."""
+        _LOGGER.debug('%s - Running update', self._name)
+        try:
+            url = BASE_URL.format(self.channel_id)
+            async with async_timeout.timeout(10, loop=self.hass.loop):
+                response = await self.session.get(url)
+                info = await response.text()
+            exp = parse(response.headers['Expires'])
+            if exp < self.expiry:
+                return
+            self.expiry = exp
+            title = html.parser.HTMLParser().unescape(
+                info.split('<title>')[2].split('</')[0])
+            url = info.split('<link rel="alternate" href="')[2].split('"/>')[0]
+            if self.live or url != self.url:
+                self.stream, self.live = await is_live(url, self._name, self.hass, self.session)
+            else:
+                _LOGGER.debug('%s - Skipping live check', self._name)
+            self.url = url
+            self.published = info.split('<published>')[2].split('</')[0]
+            thumbnail_url = info.split(
+                '<media:thumbnail url="')[1].split('"')[0]
+            self._state = title
+            self._image = thumbnail_url
+            self.stars = info.split('<media:starRating count="')[1].split('"')[0]
+            self.views = info.split('<media:statistics views="')[1].split('"')[0]
+            url = CHANNEL_LIVE_URL.format(self.channel_id)
+            self.channel_live, self.channel_image = await is_channel_live(url, self.name, self.hass, self.session)
+        except Exception as error:  # pylint: disable=broad-except
+            _LOGGER.debug('%s - Could not update - %s', self._name, error)
+
+    @property
+    def name(self):
+        """Name."""
+        return self._name
+
+    @property
+    def entity_picture(self):
+        """Picture."""
+        return self._image
+
+    @property
+    def state(self):
+        """State."""
+        return self._state
+
+    @property
+    def icon(self):
+        """Icon."""
+        return ICON
+
+    @property
+    def device_state_attributes(self):
+        """Attributes."""
+        return {'url': self.url,
+                'published': self.published,
+                'stars': self.stars,
+                'views': self.views,
+                'stream': self.stream,
+                'live': self.live,
+                'channel_is_live': self.channel_live,
+                'channel_image': self.channel_image}
+
+async def is_live(url, name, hass, session):
+    """Return bool if video is stream and bool if video is live"""
+    live = False
+    stream = False
+    try:
+        async with async_timeout.timeout(10, loop=hass.loop):
+            response = await session.get(url)
+            info = await response.text()
+        if 'isLiveBroadcast' in info:
+            stream = True
+            if 'endDate' not in info:
+                live = True
+                _LOGGER.debug('%s - Latest Video is live', name)
+    except Exception as error:  # pylint: disable=broad-except
+        _LOGGER.debug('%s - Could not update - %s', name, error)
+    return stream, live
+
+async def is_channel_live(url, name, hass, session):
+    """Return bool if channel is live"""
+    live = False
+    try:
+        async with async_timeout.timeout(10, loop=hass.loop):
+            response = await session.get(url)
+            info = await response.text()
+        if '{"iconType":"LIVE"}' in info:
+            live = True
+            _LOGGER.debug('%s - Channel is live', name)
+        regex = r"\"width\":48,\"height\":48},{\"url\":\"(.*?)\",\"width\":88,\"height\":88},{\"url\":"
+        channel_image = re.findall(regex, info, re.MULTILINE)[0].replace("=s88-c-k-c0x00ffffff-no-rj", "")
+    except Exception as error:  # pylint: disable=broad-except
+        _LOGGER.debug('%s - Could not update - %s', name, error)
+    return live, channel_image
