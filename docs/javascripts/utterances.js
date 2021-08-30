8d09cdf5a2b6a9508cc8be1d4aa9c7fe857443b4
Tiffany <hey@tiffanyrwhite.com>
2021-08-30 02:43:20 -0400

    UTTERANCES

--- docs/javascripts/utterances.js
+++ docs/javascripts/utterances.js
@@ -1,8 +1,8 @@
 <script
-  src="https://utteranc.es/client.js"
-  repo="twhite96/SuburbanSmarts"
-  issue-term="title"
-  theme="github-light"
-  crossorigin="anonymous"
-  async>
-</script>
+  src='https://utteranc.es/client.js'
+  repo='twhite96/SuburbanSmarts'
+  issue-term='title'
+  theme='github-light'
+  crossorigin='anonymous'
+  async
+></script>
